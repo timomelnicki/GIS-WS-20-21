24d555e1af086071904586cb5ffdf2bf973e4a05
Timo <timo.melnicki@hs-furtwangen.de>
2020-12-04 22:07:36 +0100

    stand 4.12

--- Typescript/Anzeige.js
+++ Typescript/Anzeige.js
@@ -1,9 +1,17 @@
 "use strict";
+function jsonlesen() {
+    let lesen = JSON.parse("BilderAuswahl.json");
+}
+async function jsondaten(_url) {
+    let response = await fetch(_url);
+    let daten = await response.json();
+    console.log("Test4", daten);
+}
 console.log("test1");
 function showTheList(json) {
-    console.log("Test3");
+    console.log("Test2");
     let arrItems;
-    arrItems = JSON.parse(json).Koepfe;
+    arrItems = json.Koepfe;
     let div = document.getElementById("Kopfbilder");
     div.innerHTML = "";
     for (let i = 0; i <= arrItems.length - 1; i++) {
@@ -16,7 +24,11 @@ function showTheList(json) {
         divRight.appendChild(img);
         div.appendChild(divLeft);
         div.appendChild(divRight);
-        console.log("Test2");
+        console.log("Test3");
     }
 }
+async function warten() {
+    await jsondaten("BilderAuswahl.json");
+}
+warten();
 //# sourceMappingURL=Anzeige.js.map
--- Typescript/Anzeige.ts
+++ Typescript/Anzeige.ts
@@ -1,16 +1,24 @@
+function jsonlesen() {
+    let lesen: Koerperteile = JSON.parse("BilderAuswahl.json");
+}
+async function jsondaten(_url: string): Promise<void> {
+    let response: Response = await fetch(_url);
+    let daten: string = await response.json();
+    console.log("Test4", daten);
+}
 interface Koerperteile {
-    Pfad: string; 
+    Pfad: string;
     Name: string;
 }
 console.log("test1");
-function showTheList(json: string): void {
-    console.log("Test3");
+function showTheList(json: any): void {
+    console.log("Test2");
     let arrItems: Koerperteile[];
-    arrItems = JSON.parse(json).Koepfe;
+    arrItems = json.Koepfe;
 
     let div: HTMLDivElement = <HTMLDivElement>document.getElementById("Kopfbilder");
     div.innerHTML = "";
-    for (let i: number = 0  ; i <= arrItems.length - 1 ; i++) {
+    for (let i: number = 0; i <= arrItems.length - 1; i++) {
         let divLeft: HTMLDivElement = document.createElement("div");
         divLeft.innerHTML = arrItems[i].Name;
 
@@ -23,6 +31,10 @@ function showTheList(json: string): void {
 
         div.appendChild(divLeft);
         div.appendChild(divRight);
-        console.log("Test2");
+        console.log("Test3");
     }
 }
+async function warten(): Promise<void> {
+    await jsondaten("BilderAuswahl.json");
+}
+warten();
